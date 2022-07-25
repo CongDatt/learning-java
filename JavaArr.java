9d772ba2d1e1016e3f7a37b020a332f41a44d074
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-07-25 23:52:08 +0700

    something

---
+++ JavaArr.java
@@ -0,0 +1,11 @@
+
+public class JavaArr {
+
+    public static void Hello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        Hello();
+    }
+}
---
+++ JavaString.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class JavaString {
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+
+        arr.add("one");
+        arr.add("two");
+        arr.add("three");
+
+        System.out.println(arr);
+    }
+}

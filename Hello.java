93da5525d708a70c0bed1609ff4294bcf0e84e9f
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-08-08 22:33:13 +0700

    add code for stack

--- Hello.java
+++ Hello.java
@@ -1,6 +1,16 @@
 class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Dat");
+        int n = 7;
+
+        while(n >= 10) {
+            n = (int) (Math.pow(n%10, 2) + Math.pow(n/10,2)); 
+            System.out.println(n);
+
+            // if(n % 100 == 0) {
+            //     System.out.println("Happy");
+            //     break;
+            // }
+        }
     }
 }

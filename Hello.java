71d89479cf72465c8db320ba4f6049d95d9239cb
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-08-13 14:00:59 +0700

    add code

--- Hello.java
+++ Hello.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Dat");
+        int n = 3;
+        String binaryNumber = "";
+
+        while(n > 0) {
+            binaryNumber = (n%2) + binaryNumber;
+            n = n / 2;
+        }
+        System.err.println(binaryNumber);
     }
 }

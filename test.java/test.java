161cb57d60bc11d2eadedeb18e97d811889e4afb
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-08-15 23:40:46 +0700

    add code for connecttion

---
+++ test.java/test.java
@@ -0,0 +1,8 @@
+import java.sql.Connection;
+
+public class test {
+    public static void main(String[] args) {
+        Connection connection = JDBCUtil.getConnection();
+        System.out.println(connection);
+    }
+}

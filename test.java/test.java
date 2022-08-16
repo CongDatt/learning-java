33ad178a196c26491a13fc716d7be4cfe72335d8
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-08-16 21:13:42 +0700

    add code

--- test.java/test.java
+++ test.java/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         Connection connection = JDBCUtil.getConnection();
         System.out.println(connection);
+        System.out.println("hello");
     }
 }

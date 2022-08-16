87bb1afc50ef131ec60cd028c70e6e225d66c2f4
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-08-16 23:20:37 +0700

    add code

--- test.java/test.java
+++ test.java/test.java
@@ -1,8 +1,31 @@
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class test {
     public static void main(String[] args) {
-        Connection connection = JDBCUtil.getConnection();
-        System.out.println(connection);
+        try {
+            // B1: Tao Ket noi
+            Connection connection = JDBCUtil.getConnection();
+
+            // B2: Tao doi tuong statement
+            Statement statement = connection.createStatement();
+
+            // B3: Xu ly insert data
+            String sql = "INSERT INTO persons(last_name, first_name, gendetn, dob, income)"
+                    + "VALUES (\"Tran\", \"Thi\", \"C\", \"2000-09-10\", 10000000)";
+
+            // B4: Show Kq
+            int check = statement.executeUpdate(sql);
+            System.out.println("So dong thay doi: "  + check);
+            if(check > 0) {
+                System.out.println("Updated !!!!");
+            }
+
+            // B5: Dong ket noi
+            JDBCUtil.closeConnection(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

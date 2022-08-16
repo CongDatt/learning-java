23595f22ad9454d4a060b1d2e82b12c76ede09b8
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-08-16 21:58:40 +0700

    add code

--- test.java/test.java
+++ test.java/test.java
@@ -1,8 +1,17 @@
 import java.sql.Connection;
+import java.util.Arrays;
 
 public class test {
     public static void main(String[] args) {
         Connection connection = JDBCUtil.getConnection();
-        System.out.println(connection);
+
+        int[] a = new int[] {1,0,4,4,5,7,8};
+        int[] b = new int[10];
+
+        Arrays.sort(a);
+        Arrays.fill(b,10);
+        System.out.println("Mang a la: " + Arrays.toString(a));
+        System.out.println("Search: " + Arrays.binarySearch(a,4));
+        System.out.println("Mang b la: " + Arrays.toString(b));
     }
 }

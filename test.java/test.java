72967c200d17392660dc23e91276a070f3259424
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-08-18 13:37:14 +0700

    add code

--- test.java/test.java
+++ test.java/test.java
@@ -1,8 +1,20 @@
 import java.sql.Connection;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class test {
     public static void main(String[] args) {
-        Connection connection = JDBCUtil.getConnection();
-        System.out.println(connection);
+        Scanner sc = new Scanner(System.in);
+
+        Stack<String> stack = new Stack<>();
+        System.out.println("Nhap chuoi vao: ");
+        String s = sc.nextLine();
+        for(int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i)+"");
+        }
+        System.out.println("Chuoi dao nguoc: ");
+        for(int i = 0; i < s.length(); i++) {
+            System.out.print(stack.pop());
+        }
     }
 }

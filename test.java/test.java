df26cbc8e46548e6a86f0b0fcc9a73cd58c35030
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-08-20 23:32:30 +0700

    add code

--- test.java/test.java
+++ test.java/test.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 
 public class test {
     public static void main(String[] args) {
-        Connection connection = JDBCUtil.getConnection();
-        System.out.println(connection);
+        Box a = new Box<Integer>(10);
+        System.out.println(a.getValue());
     }
 }

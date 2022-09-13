28b3b1ac49699b9b6460e59a4106a07202908c4e
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-09-13 23:44:57 +0700

    String pool vs String heap

---
+++ test.java/JavaString.java
@@ -0,0 +1,10 @@
+public class JavaString {
+    public static void main(String[] args) {
+        String a = "a";
+        String aa = "a"; // store in String heap
+        String a2 = new String("a"); // store in String pool
+
+        System.out.println(a == aa); // true
+        System.out.println(a == a2); // false
+    }
+}
--- test.java/test.java
+++ test.java/test.java
@@ -1,8 +1,16 @@
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class test {
     public static void main(String[] args) {
-        Connection connection = JDBCUtil.getConnection();
-        System.out.println(connection);
+        ArrayList<String> users = new ArrayList<String>();
+
+        users.add("Hello");
+        users.add("Guys");
+
+        for (String user : users) {
+            System.out.println(user);
+        }
     }
 }

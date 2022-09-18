2b40fbfed73e7d0c31392dd0335e03af06f71a82
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-09-18 13:07:51 +0700

    addd

--- test.java/test.java
+++ test.java/test.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class test {
@@ -10,14 +12,21 @@ public class test {
         users.add("Guys");
 
         for (String user : users) {
-            System.out.println(user);
+//            System.out.println(user);
         }
 
         myGenerics<String> a = new myGenerics<String>("string a");
         myGenerics<Integer> aNum = new myGenerics<>(10);
 
-        System.out.println(a.getValue());
-        System.out.println(aNum.getValue());
+//        System.out.println(a.getValue());
+//        System.out.println(aNum.getValue());
 
+        List<Person> people = Arrays.asList(new Person("John", "Doe"),
+                new Person("Bob", "Dole"),
+                new Person("Ronald", "McDonald"),
+                new Person("Alice", "McDonald"),
+                new Person("Jill", "Doe"));
+        Collections.sort(people);
+        System.out.println(people);
     }
 }

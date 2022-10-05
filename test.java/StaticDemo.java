4b881a29d2e610ed80da34eb6220dfbc5f3dde2b
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-10-05 17:31:06 +0700

    dd

--- test.java/StaticDemo.java
+++ test.java/StaticDemo.java
@@ -1,6 +1,18 @@
+// int / float / double
+// Integer
+
 public class StaticDemo {
 
     public static void main(String[] args) {
+        int i = 5; // Primitive data type -> int / float / double
+        Integer ii = new Integer(3); // Wrapper class - Boxing
+        Double doubleee = 5.5; // AutoBoxing
+        int j = ii.intValue(); // unboxing - unwrapping
 
+
+        System.out.println(i);
+        System.out.println(ii);
+        System.out.println(doubleee);
+        System.out.println(j);
     }
 }

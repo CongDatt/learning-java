5e5b4431228c12586241a85fa9dab3a96d15a66f
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-10-29 09:19:33 +0700

    add code

--- test.java/test.java
+++ test.java/test.java
@@ -1,8 +1,21 @@
-import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class test {
     public static void main(String[] args) {
-        Connection connection = JDBCUtil.getConnection();
-        System.out.println(connection);
+
+        List<String> myList = new ArrayList<>();
+        List<String> myListTwo = new ArrayList<>();
+        List<String> myListThree = new ArrayList<>();
+
+        myList.add("1");
+        myListTwo.add("2");
+
+        myListThree.addAll(myListTwo);
+        myListThree.addAll(myList);
+
+        System.out.println(myListThree);
+
     }
 }
